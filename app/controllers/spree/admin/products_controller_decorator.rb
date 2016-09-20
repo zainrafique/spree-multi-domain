87c130e03ceaba93976ece2b52ca7c82f67293f2
Arnaud Augier <arnaud.augier@gmail.com>
2016-09-20 18:16:50 +0200

    allow multiple taxons in product
    Allow to affect multiple taxons in product when using spree-multi-domain : see #148

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,17 +1,18 @@
 Spree::Admin::ProductsController.class_eval do
   update.before :set_stores
-
-  def update
-    store_ids = params[:product][:store_ids]
-    if store_ids.present?
-      params[:product][:store_ids] = store_ids.split(',')
-    end
-    super
-  end
+  before_action :find_stores, only: [:update]
 
   private
 
   def set_stores
     @product.store_ids = nil unless params[:product].key? :store_ids
   end
+  
+  def find_stores
+    store_ids = params[:product][:store_ids]
+    if store_ids.present?
+      params[:product][:store_ids] = store_ids.split(',')
+    end
+  end
+
 end
