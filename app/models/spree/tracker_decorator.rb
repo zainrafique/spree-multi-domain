bf7171638b45f17fe1a4ce64703c92fb92ed989c
Damian Legawiec <damian@sparksolutions.co>
2018-01-22 17:24:42 +0100

    In Spree 3.5 trackers were extracted to a separate extension
    github.com/spree-contrib/spree_analytics_trackers

--- app/models/spree/tracker_decorator.rb
+++ app/models/spree/tracker_decorator.rb
@@ -1,7 +1,9 @@
-Spree::Tracker.class_eval do
-  belongs_to :store
+if defined?(Spree::Tracker)
+  Spree::Tracker.class_eval do
+    belongs_to :store
 
-  def self.current(domain)
-    Spree::Tracker.where(active: true).joins(:store).where("spree_stores.url LIKE ?", "%#{domain}%").first
+    def self.current(domain)
+      Spree::Tracker.where(active: true).joins(:store).where("spree_stores.url LIKE ?", "%#{domain}%").first
+    end
   end
 end
