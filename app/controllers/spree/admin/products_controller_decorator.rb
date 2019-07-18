ba00b06adcf6daba8f26c401992407a8dda89c3d
Piotr Milewski <piotr.milewski@sparksolutions.co>
2019-07-18 14:51:40 +0200

    fix update properties destroys stores association

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,13 +1,8 @@
 Spree::Admin::ProductsController.class_eval do
-  update.before :set_stores
   before_action :find_stores, only: [:update]
 
   private
 
-  def set_stores
-    @product.store_ids = nil unless params[:product].key? :store_ids
-  end
-  
   def find_stores
     store_ids = params[:product][:store_ids]
     if store_ids.present?
