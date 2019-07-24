75cd901ea836d1f31fb9e11fbfcf79b1cc7063e0
Piotr Milewski <piotr.milewski@sparksolutions.co>
2019-07-24 14:01:33 +0200

    products sidebar taxonomies

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -2,6 +2,13 @@ Spree::ProductsController.class_eval do
 
   before_action :can_show_product, only: :show
 
+  def index
+    @searcher = build_searcher(params.merge(include_images: true))
+    @products = @searcher.retrieve_products
+    @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
+    @taxonomies = get_taxonomies
+  end
+
   private
 
   def can_show_product
