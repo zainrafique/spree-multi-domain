a046b877e89c42e1d7dc0cd8a43d92e784cfd02b
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-11-29 10:48:09 +0100

    Corrects requests specs

--- spec/requests/global_controller_helpers_spec.rb
+++ spec/requests/global_controller_helpers_spec.rb
@@ -1,63 +1,58 @@
 require 'spec_helper'
 
 describe "Global controller helpers" do
+  let!(:store)   { create :store }
+  let!(:tracker) { create :tracker, store: store }
 
-  let!(:store) { FactoryBot.create :store }
+  before { get '/' }
 
-  before(:each) do
-    @tracker = FactoryBot.create :tracker, store: store
-    get '/'
+  it 'should include the right tracker' do
+    expect(response.body).to include(tracker.analytics_id)
   end
 
-  it "should include the right tracker" do
-    response.body.should include(@tracker.analytics_id)
+  it 'should create a store-aware order' do
+    expect(controller.current_store).to eq(store)
   end
 
-  it "should create a store-aware order" do
-    controller.current_store.should == store
-  end
-
-  it "should instantiate the correct store-bound tracker" do
-    controller.current_tracker.should == @tracker
+  it 'should instantiate the correct store-bound tracker' do
+    expect(controller.current_tracker).to eq(tracker)
   end
 
   describe '.current_currency' do
     subject { controller.current_currency }
 
-    context "when store default_currency is nil" do
-      it { should == 'USD' }
-    end
-
-    context "when the current store default_currency empty" do
-      let!(:store) { FactoryBot.create :store, default_currency: '' }
-
-      it { should == 'USD' }
-    end
-
-    context "when the current store default_currency is a currency" do
-      let!(:store) { FactoryBot.create :store, default_currency: 'EUR' }
-      it { should == 'EUR' }
-    end
-
-    context "when session[:currency] set by spree_multi_currency" do
-
-      before do
-        session[:currency] = 'AUD'
+    context 'when store default_currency is not defined' do
+      it 'equals USD' do
+        expect(subject).to eq('USD')
       end
+    end
 
-      let!(:aud) { ::Money::Currency.find('AUD') }
-      let!(:eur) { ::Money::Currency.find('EUR') }
-      let!(:usd) { ::Money::Currency.find('USD') }
-      let!(:store) { FactoryBot.create :store, default_currency: 'EUR' }
+    context 'when the current store default_currency is a currency' do
+      let!(:store) { create(:store, default_currency: 'EUR') }
+
+      it 'is equal EUR' do
+        expect(subject).to eq('EUR')
+      end
+    end
+
+    context 'when session[:currency] set by spree_multi_currency' do
+      before { session[:currency] = 'AUD' }
+
+      let!(:aud)   { ::Money::Currency.find('AUD') }
+      let!(:eur)   { ::Money::Currency.find('EUR') }
+      let!(:usd)   { ::Money::Currency.find('USD') }
+      let!(:store) { create(:store, default_currency: 'EUR') }
 
       it 'returns supported currencies' do
-        controller.stub(:supported_currencies).and_return([aud, eur, usd])
-        expect(controller.current_currency).to eql('AUD')
+        allow(controller).to receive(:supported_currencies).and_return([aud, eur, usd])
+
+        expect(controller.current_currency).to eq('AUD')
       end
 
       it 'returns store currency if not supported' do
-        controller.stub(:supported_currencies).and_return([eur, usd])
-        expect(controller.current_currency).to eql('EUR')
+        allow(controller).to receive(:supported_currencies).and_return([eur, usd])
+
+        expect(controller.current_currency).to eq('EUR')
       end
     end
   end
